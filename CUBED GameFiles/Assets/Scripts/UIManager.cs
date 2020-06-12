f95cef1dfee6fec0b3acbddd5e35bee544a6103a
Raine <bubblepuppy2017@gmail.com>
2020-06-12 12:53:12 -0400

    Fixed broken commit

---
+++ CUBED GameFiles/Assets/Scripts/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] loadMenu;
+
+    [SerializeField]
+    GameObject[] settings;
+
+    [SerializeField]
+    GameObject[] mainMenu;
+
+    void Start()
+    {
+
+    }
+
+    void StartClicked()
+    {
+        Debug.Log("Started");
+        CloseMenu(mainMenu);
+        OpenMenu(loadMenu);
+        Debug.Log("Arrived here");
+    }
+
+    void SettingsClicked()
+    {
+        CloseMenu(mainMenu);
+        OpenMenu(settings);
+        
+    }
+
+    private void CloseMenu(GameObject[] menu)
+    {
+        foreach (GameObject widget in menu)
+        {
+            widget.SetActive(false);
+        }
+    }
+
+    private void OpenMenu(GameObject[] menu)
+    {
+        foreach (GameObject widget in menu)
+        {
+            widget.SetActive(true);
+        }
+    }
+}
