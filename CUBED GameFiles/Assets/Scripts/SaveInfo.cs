6d8d9bacde235aeaccefbefa96188ab6f71eecbb
Raine <bubblepuppy2017@gmail.com>
2020-06-12 16:19:28 -0400

    Added more UI stuff

---
+++ CUBED GameFiles/Assets/Scripts/SaveInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SaveInfo : MonoBehaviour
+{
+    Text usertext;
+
+    private void SetUsername()
+    {
+        usertext = gameObject.GetComponent<Text>();
+
+        string username = usertext.text.ToString();
+        PlayerPrefs.SetString("username", username);
+        PlayerPrefs.Save();
+    }
+}
---
+++ CUBED GameFiles/Assets/UserPrint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UserPrint : MonoBehaviour
+{
+    TextMeshProUGUI user;
+    string username;
+
+    void Start()
+    {
+        user = gameObject.GetComponent<TextMeshProUGUI>();
+        username = PlayerPrefs.GetString("username");
+        user.SetText(username);
+    }
+}
